3a98df062c4411f19b1a21b2e2aa2babca461057
DiegoLibonati <diego.libonati1998@gmail.com>
2024-05-17 01:16:29 -0300

    Multiple keys are now allowed to be entered through the use of: | to make a single object where many keys perform the same function.

--- src/useKeyBoard.tsx
+++ src/useKeyBoard.tsx
@@ -5,7 +5,25 @@ export const useKeyBoard = ({ config }: UseKeyBoardProps): UseKeyBoard => {
   const { keys, debug, dependencies } = config;
 
   const onKeyPress = useCallback((e: KeyboardEvent) => {
-    keys.map((element) => {
+    keys.forEach((element) => {
+      if (element.key === "|" && e.key === element.key) {
+        element.fn();
+        return;
+      }
+
+      if (element.key.includes("|")) {
+        const multipleKeys = element.key.split("|").filter((key) => key);
+
+        if (multipleKeys.length < 2) {
+          return;
+        }
+
+        if (multipleKeys.includes(e.key)) {
+          element.fn();
+        }
+        return;
+      }
+
       if (e.key === element.key) {
         element.fn();
         return;
@@ -23,13 +41,44 @@ export const useKeyBoard = ({ config }: UseKeyBoardProps): UseKeyBoard => {
 
   useEffect(() => {
     if (debug && keys.length > 0) {
-      keys.map((element) =>
+      keys.forEach((element) => {
+        if (element.key === "|") {
+          console.log(
+            `Key %c${element.key}%c loaded ✅.`,
+            "color: #09f",
+            "color: initial"
+          );
+          return;
+        }
+
+        if (element.key.includes("|")) {
+          const multipleKeys = element.key.split("|").filter((key) => key);
+
+          if (multipleKeys.length < 2) {
+            console.log(
+              `If you use the key field with | it is to pass multiple keys with the same function. I regret to inform you that the key %c${element.key}%c was not loaded successfully ❌.`,
+              "color: red",
+              "color: initial"
+            );
+            return;
+          }
+
+          multipleKeys.forEach((key) =>
+            console.log(
+              `Key %c${key}%c loaded ✅.`,
+              "color: #09f",
+              "color: initial"
+            )
+          );
+          return;
+        }
+
         console.log(
           `Key %c${element.key}%c loaded ✅.`,
           "color: #09f",
           "color: initial"
-        )
-      );
+        );
+      });
       return;
     }
 
